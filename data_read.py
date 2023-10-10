7ccdda95377740214392a6ca5dc7cfae47fab7b8
Tutoshniy <gleb.gadbrat@mail.ru>
2023-10-10 12:27:35 +0300

    redact data_read

--- data_read.py
+++ data_read.py
@@ -2,6 +2,8 @@ def read_notes():
     notes = {}
     file = open("notes.txt", "r")
     for line in file.readlines():
+        if line == '\n':
+            continue
         elements = line.split(":")
         notes[elements[0]] = elements[1]
     return notes
